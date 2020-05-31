73d326a8079fed6c1d0fd68c1c22faeb766daee7
Pranay Chauhan <52269813+PranayChauhan2516@users.noreply.github.com>
2020-06-01 02:45:14 +0530

    DIEHARD Spoj
    Greedy Easy

---
+++ DIEHARD.java
@@ -0,0 +1,145 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Pranay2516
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        FastReader in = new FastReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class Task {
+        public void solve(int testNumber, FastReader in, PrintWriter out) {
+            int h = in.nextInt() + 3, a = in.nextInt() + 2, cnt = 1, f = 0;
+            while (h > 0 && a > 0) {
+                if (f != 0) {
+                    h += 3;
+                    a += 2;
+                    f = 0;
+                } else if (h > 5 && a > 10 && f != 1) {
+                    h -= 5;
+                    a -= 10;
+                    f = 1;
+                } else if (h > 20 && f != 2) {
+                    h -= 20;
+                    a += 5;
+                    f = 2;
+                } else break;
+                cnt++;
+            }
+            out.println(cnt);
+        }
+
+    }
+
+    static class FastReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private FastReader.SpaceCharFilter filter;
+
+        public FastReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+
+            if (numChars == -1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars) {
+
+                curChar = 0;
+
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+
+                if (numChars <= 0)
+                    return -1;
+            }
+
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public String next() {
+
+            int c = read();
+
+            while (isSpaceChar(c))
+                c = read();
+
+            StringBuilder res = new StringBuilder();
+
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+
+            if (filter != null)
+                return filter.isSpaceChar(c);
+
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
